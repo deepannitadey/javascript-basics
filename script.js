3e12728503cc822d31d539f96a25770195e7ae9f
Deepannita Dey <deepz.ria@gmail.com>
2020-06-02 21:11:29 +0530

    Use variables in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+var firstName = "Deepannita";
+console.log(firstName);
 
+var lastName = "Dey";
+console.log(lastName);
+
+var age = 24.0;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job = "Coder";
+console.log(job);
+
+var gender;
+console.log(gender);
